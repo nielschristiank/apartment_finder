11d4c73e69c06576a3ea794d72bab574cf1a3deb
Niels-Christian Kielland <nielschristian@NielsChtiansMBP.localdomain>
2018-09-19 14:16:27 -0700

    added apartment table to database

---
+++ db/migrate/20180919203454_create_apartments.rb
@@ -0,0 +1,17 @@
+class CreateApartments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :apartments do |t|
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.integer :postal_code
+      t.string :state
+      t.string :country
+      t.string :building_manager_name
+      t.bigint :building_manager_phone
+      t.text :building_manager_hours
+
+      t.timestamps
+    end
+  end
+end
