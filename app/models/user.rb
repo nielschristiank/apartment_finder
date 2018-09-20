e929013de48f78c1bf5d837ae28ae369b2a2f50c
Niels-Christian Kielland <nielschristian@NielsChtiansMBP.localdomain>
2018-09-20 11:34:41 -0700

    reworked models and schema: User has many apartments, apartments belongs to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :apartments, dependent: :destroy
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
