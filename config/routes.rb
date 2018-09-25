e769e27c32d1bc4b895bfb37d7bb1dd5bd287275
Niels-Christian Kielland <nielschristian@NielsChtiansMBP.localdomain>
2018-09-24 17:06:23 -0700

    added backend route to get all apartments created by user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :apartments
-  devise_for :users, defaults: {format: :json}, controllers: {registrations: "users/registrations" }
+  get '/apartments/user/:user_id' => 'apartments#show_by_user'
+  devise_for :users, defaults: {format: :json}, controllers: { sessions: 'users/sessions', registrations: "users/registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
