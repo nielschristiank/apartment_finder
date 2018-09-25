e5881f47c67df8f55b336f81e767686360ebbbb6
Niels-Christian Kielland <nielschristian@NielsChtiansMBP.localdomain>
2018-09-25 16:10:43 -0700

    updated user apartments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :apartments
-  get '/apartments/user/:user_id' => 'apartments#show_by_user'
+  get '/user/:user_id/apartments' => 'apartments#show_by_user'
   devise_for :users, defaults: {format: :json}, controllers: { sessions: 'users/sessions', registrations: "users/registrations" }
   get '/users/:id' => 'information#show_user'
+
+  # resources :user do
+  #   resources :apartments
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
